7fac5364c19648526f2bf47bab2b1bce10f6df98
Oscar-373 <oscarglez2b@gmail.com>
2022-06-07 17:48:14 -0500

    Se termino el proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import UsersForm from './components/UsersForm';
+import UsersList from './components/UsersList';
 
 function App() {
+
+  const [ users, setUsers ] = useState([]); 
+  const [ userSelected, setUserSelected ] = useState(null);
+
+  useEffect(() => {
+   axios.get('https://users-crud1.herokuapp.com/users/')
+      .then(res => setUsers(res.data))
+  }, []);
+
+  const getUsers = () => {
+    axios.get('https://users-crud1.herokuapp.com/users/')
+      .then(res => setUsers(res.data))
+  }
+  
+  const selectUser = user => setUserSelected(user)
+
+  const deselectUser = () => setUserSelected(null);
+
+  const deleteUser = id => {
+    axios.delete(`https://users-crud1.herokuapp.com/users/${id}/`)
+    .then(() => getUsers());
+  }
+
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <UsersForm  deselectUser= {deselectUser} getUsers = {getUsers} userSelected = {userSelected}  />
+      <UsersList users={users} selectUser={selectUser} deleteUser={deleteUser} />
+     
     </div>
   );
 }
